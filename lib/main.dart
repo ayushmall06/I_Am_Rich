1a34bb4afddd5614d18cf90638b7add3d2c1ba3a
Ayush Mall <ayushmall@Ayushs-MacBook-Pro.local>
2021-06-07 17:43:28 +0530

    Day 2 Committed

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,15 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-
+        backgroundColor: Colors.blueGrey[50],
         appBar: AppBar(
           title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
-        body: Image(
-          image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
         ),
       ),
     ),
